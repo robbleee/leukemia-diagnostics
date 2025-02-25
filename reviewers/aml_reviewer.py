32f175fd89a5906b8ed4c64a7e59ded94fa832e1
Robert Lee <49095226+robbleee@users.noreply.github.com>
2025-02-25 12:39:47 +0000

    added new prompts

--- reviewers/aml_reviewer.py
+++ reviewers/aml_reviewer.py
@@ -69,15 +69,15 @@ Derivation: {icc_deriv}
 **Manual inputs**: {manual_inputs}
 **Classification Result**: {classification_text}
 
-**Task**: 
-1. Using the heading: “Classification Review”. State any significant differences in the classification of this case given by WHO and ICC focussing particularly on the genetic elements of this classification if present. 
-2. Using the heading: “Sample Quality”. If genetic testing or cytogenetic test results are not present in the provided data, then state any impact this may have on the classification. If these are stated in the reviewed information, please discuss the sample quality statement for clinical report, DNA quality metric and the cytogenetics report. Based on each of these values discuss any concerns around sample quality. If the morphology report suggests poor sample quality, then consider whether this may affect the representation of cells in the genetic sample and how it may affect VAF or sensitivity. 
+**Task**:
+1. Using the heading: “Classification Review”. State any significant differences in the classification of this case given by WHO and ICC focussing particularly on the genetic and cytogenetic elements of this classification if present.
+2. Using the heading: “Sample Quality”. If genetic testing or cytogenetic test results are not present in the provided data, then state any impact this may have on the classification. If these are stated in the reviewed information, please discuss the sample quality statement for clinical report, DNA quality metric and the cytogenetics report. Based on each of these values discuss any concerns around sample quality. If the morphology report suggests poor sample quality, then consider whether this may affect the representation of cells in the genetic sample and how it may affect VAF or sensitivity.
 
-**Response**: 
-- Be concise and professional. Provide only the headings stated. Headings should be in bold type followed by a colon, then the text should follow on the next line. - Use UK English spelling. The text elements should not use bold font at any point. 
+**Response**:
+- Be concise and professional. Provide only the headings stated. Headings should be in bold type followed by a colon, then the text should follow on the next line. - Use UK English spelling. The text elements should not use bold font at any point.
 - Format in Markdown with smaller headings (**<heading**>) for a Streamlit UI.
 
-- Response should not be more than 150 words. 
+- Response should not be more than 150 words.
 """
 
 
@@ -133,12 +133,12 @@ def get_gpt4_review_aml_genes(classification: dict,
 
 **Task**:
 Provide a section called Genetics Review.
-Please follow these rules: 
+Please follow these rules:
 1. Use UK spelling.  Whenever a gene name is used this should be stated in capital letters and italic text irrespective of any other instruction.
-2. If there are no mutated genes or cytogenetic change present then do not discuss any genes. Instead state that no genetic or cytogenetic lesions were detected using the procedures and panels employed in the testing and advise that the classification has been made on that basis. Suggest that MDT meetings should advise whether repeat or extended testing should be performed.
-3. Where genetic or cytogenetic lesions are found summarise the clinical implications for each positive finding in the above list. This discussion should assume a proven diagnosis of AML. The summary for each gene should use fewer than 200 words and be written to inform a medical professional using succinct language and only using peer reviewed content. The summary should emphasise the role of the listed genes or cytogenetic change on clinical outcome. 
-4. If outcome effects may be modified by other genes indicate this in bold lettering (except for gene names which remain in italic capital text). This action should consider only the effects of any other genes on the provided input list. 
-6. Provide three references that have high citation for each gene. 
+2. If there are no mutated genes or cytogenetic changes present then do not discuss any genetic or cytogenetic results. Instead state that no genetic or cytogenetic lesions were detected using the procedures and panels employed in the testing and advise that the classification has been made assumingthere are no cytogenetic lesions or genetic lesions present. Suggest that MDT meetings should review the results and advise whether repeat or extended testing should be performed to ensure that this is the correct result.
+3. Where genetic or cytogenetic lesions are found summarise the clinical implications for each positive genetic or cytogenetic result reported for this case. This discussion should assume a proven diagnosis of AML. The summary for each gene should use fewer than 200 words and be written to inform a medical professional using succinct language and only using peer reviewed content. The summary should emphasise the role of the listed genes or cytogenetic change on clinical outcome.
+4. Provide three references that have high citation for each gene.
+5. If outcome effects may be modified by other genes reported to be mutated in this case then indicate this in bold lettering (except for gene names which remain in italic capital text). This action should consider only genes on the provided input list and use only findings from highly-cited journals. If any co-mutations present in the case have a significant affect on outcome please provide one useful reference from a highly cited journal to support this.
 
 
 **Response**:
@@ -198,19 +198,19 @@ def get_gpt4_review_aml_mrd(classification: dict,
 **Task**:
 Provide a section called MRD strategy
 
-Please follow these rules: 
-1. Use only the gene and cytogenetic lists from this input data. 
-2. Use UK spelling. Whenever a gene name is used this should be stated in capital letters and italic text irrespective of any other instruction. 
-2. Discuss only those genes from the list that are suitable for monitoring minimal residual disease in the UK [VERY IMPORTANT - ONLY INCLUDE GENES THAT CAN BE USED IN THE UK]. 
-3. Then for any gene that can be used to monitor MRD advise the appropriate monitoring recommendations used in the UK. The advice should be provided for a well-informed doctor wishing to monitor the patient and should be succinct but include time intervals and sample types. This should be performed separately for each identified target gene or cytogenetic lesion. The monitoring recommendation should use European LeukemiaNet MRD Working Party recommendations 2021 described in Blood. 2021 Dec 30;138(26):2753–2767. The summary for each gene should use fewer than 200 words and be written to inform a medical professional using succinct language and only using peer reviewed content. Do not mention genes present in the list but not detected in this case. 
-4. If a cytogenetic lesion is recommended for monitoring as a marker of disease response or can be used as such then discuss that cytogenetic lesion too. 
-5. Provide a maximum of 2 references that have high citation for each recommendation. 
+Please follow these rules:
+1. Use only the gene and cytogenetic lists from this input data.
+2. Use UK spelling. Whenever a gene name is used this should be stated in capital letters and italic text irrespective of any other instruction.
+2. Discuss only those genes from the list that are suitable for monitoring minimal residual disease in the UK [VERY IMPORTANT - ONLY INCLUDE GENES THAT ARE RECOMMENDED FOR MRD MONITORING IN THE UK].
+3. Then for any gene that fulfils the criteria for monitoring of MRD in the UK, advise the appropriate monitoring recommendations used in the UK. The advice should be provided for a well-informed doctor wishing to monitor the patient and should be succinct but include time intervals and sample types. This should be performed separately for each identified target gene or cytogenetic lesion. The monitoring recommendation should use European LeukemiaNet MRD Working Party recommendations 2021 described in Blood. 2021 Dec 30;138(26):2753–2767. The summary for each gene should use fewer than 200 words and be written to inform a medical professional using succinct language and only using peer reviewed content. Do not mention genes present in the list but not detected in this case.
+4. If a cytogenetic lesion is recommended for monitoring as a marker of disease response or can be used as such then discuss that cytogenetic lesion too. Use fewer than 150 words.
+5. Provide a maximum of 2 references that have high citation for each recommendation.
 
-**Response**: 
-- Structure your answer beautifully in markdown with smaller headings (**<heading**>) for a Streamlit UI. - Make sure that the individual gene headers are on their own line 
-- Do not ever include anything like this "Certainly, here is the Genetics Review based on the provided data:" 
+**Response**:
+- Structure your answer beautifully in markdown with smaller headings (**<heading**>) for a Streamlit UI. - Make sure that the individual gene headers are on their own line
+- Do not ever include anything like this "Certainly, here is the Genetics Review based on the provided data:"
 - Do not attempt to provide an overview summary after the written sections
-- Do not provide suggestions about treatment approaches or general statements about the value of monitoring MRD 
+- Do not provide suggestions about treatment approaches or general statements about the value of monitoring MRD
 - When structuring your response place those mutations that are suitable for MRD monitoring first in your output 
 - If there are no positive gene findings to note then just say that, don't put put in genes that aren't there.
 """
@@ -260,11 +260,12 @@ def get_gpt4_review_aml_additional_comments(classification: dict,
 **Classification Result**: {classification}
 
 **Task**:
-	Provide a section called Additional Comments
-	Using "Additional Comments" as the main heading, please follow these rules:  
-	1. Use UK spelling.  Whenever a gene name is used this should be stated in capital letters and italic text irrespective of any other instruction.
-	2. Use the subtitle: "Possible germline origin of mutations:" answer the following query: This is a bone marrow sample and the patient is known to have acute myeloid leukaemia. You are offering advice to an expert haematologist. Do not discuss treatment and make your answer concise using no subheadings and fewer than 150 words and a single paragraph without underlining any words. For any gene where a germline mutation is possible and the VAF % could support a germline mutation then discuss likelihood of this for that gene or genes only. Do not offer advice on further testing [VERY IPORTANT - COMMENT ON VAF SPECIFICALLY MENTIONING THE % VAF]
-	3. For the same gene list and VAF use a new paragraph titled: "Possibility of lymphoid clonality within the sample:" Identify any genes that have recognised mutation in lymphoid cells. Using the overall VAF levels consider whether there is a significant possibility of lymphoid gene mutation being present in the bone marrow sample [VERY IPORTANT - COMMENT ON VAF SPECIFICALLY MENTIONING THE % VAF]
+    Provide a section called Additional Comments
+    Using "Additional Comments" as the main heading, please follow these rules: 
+    1. Use UK spelling.  Whenever a gene name is used this should be stated in capital letters and italic text irrespective of any other instruction.
+    2. Use the subtitle: "Possible germline predisposition for AML of mutations found in this case" answer the following query: This is a bone marrow sample and the patient is known to have acute myeloid leukaemia. You are offering advice to an expert haematologist. Do not discuss treatment and make your answer concise. For any gene where a germline predisposition to AML is recognised in highly-cted peer reviewed journals state the gene name. If the reported VAF level of the gene in this case is >35% suggest that a germline mutation is possible and if clinically appropriate this should be excluded. If no genes fit this critereon then omit this section entirely, omit both the title and content and offer no comment.
+    3. Use the subtitle: "Possible lymphoid origin of mutations found in this case" answer the following query: This is a bone marrow sample. You are offering advice to an expert haematologist. Do not discuss treatment. Make your answer concise. FOR THIS PART ONLY IGNORE THE FACT THAT THE PATIENT HAS AML. For each mutated gene consider whether it has possible lymphoid origin: this comment should consider if any of the mutated genes may occur in association with lymphoid neoplasms. If so then please state the gene name and VAF and advise that this may be considered if clinical features fit.  If no genes fit this critereon then omit this section entirely, omit the title and content and offer no comment. 
+    5. If there are no sections to include in this "Additional Comments" section then write "There are no additional comments to include"
 
 **Response**:
     - Structure your answer beautifully in markdown with smaller headings (**<heading**>) for a Streamlit UI..
